16463fc524bd4804deb44a89b1324f6916e41002
Yash Verma <yash.silver2110@gmail.com>
2023-12-04 22:54:39 +0530

    fist commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,10 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  daisyui: {
+    themes: ['light', 'forest'],
   },
-  plugins: [],
-}
+};
+
 export default config
